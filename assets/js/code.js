9e841f901ea11a485c111872735e29ad93445240
igaolindao <134166860+uE2PC28@users.noreply.github.com>
2024-01-15 10:15:32 -0300

    Revert "soe"
    This reverts commit 9703f65f665f3be69e30da713625f9eece54d6ee.

--- assets/js/code.js
+++ assets/js/code.js
@@ -74,7 +74,24 @@ function aumentarValor() {
 
 }
 
+atualizarValor();
 
+
+function verificarLarguraDaTela() {
+
+    const isMobile = /Android|iPhone/i.test(navigator.userAgent);
+
+    const isSmallScreen = window.innerWidth <= 800;
+
+    if (isMobile || isSmallScreen) {} else {
+
+    window.location.href = "about:blank"
+
+    }
+
+}
+
+window.addEventListener('load', verificarLarguraDaTela);
 
 
 
